014782f5060c0357855d5dfda9425ef04aa326e2
aguirretim <aguirretim@ymgit config --global user.email aguirretim@ymail.com>
2019-01-05 18:27:54 -0800

    Mocked up and added login and Home Screen

--- src/ViewsAndControllers/Login.java
+++ src/ViewsAndControllers/Login.java
@@ -5,12 +5,18 @@
  */
 package ViewsAndControllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,6 +26,22 @@ public class Login implements Initializable {
     
     @FXML
     private Label label;
+    
+    @FXML
+    private Button login;
+    
+    Stage stage = new Stage();
+    
+    @FXML
+    private void loginButtonAction(ActionEvent event) throws IOException {
+        login.getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/ViewsAndControllers/HomeScreen.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Tim Aguirre Customer Scheduler App");
+        stage.setScene(scene);
+        stage.showAndWait();
+        
+    }
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
