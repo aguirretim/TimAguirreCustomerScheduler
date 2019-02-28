352bfc6e546c2c0e719e9cdba1d5aa20acb42317
aguirretim <aguirretim@ymgit config --global user.email aguirretim@ymail.com>
2019-02-27 22:15:18 -0800

    Validation Added to Create and Edit Customer

--- src/ViewsAndControllers/AddCustomerController.java
+++ src/ViewsAndControllers/AddCustomerController.java
@@ -13,13 +13,17 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
@@ -67,6 +71,12 @@ public class AddCustomerController implements Initializable {
 
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
+    Locale mexicoLocale = new Locale("es", "MX");
+
+    //Changes default language english to testing language 
+    //Locale.setDefault(mexicoLocale);
+    Locale currentLocale = Locale.getDefault();
+
     /**
      * ***********************************
      * Changing screens and scenes with buttons.
@@ -83,7 +93,8 @@ public class AddCustomerController implements Initializable {
 
     @FXML
     private void saveButtonAction(ActionEvent event) throws IOException {
-        try {
+        if (validateAll()) {
+            try {
             int userId = Login.getLoggedInUserId();
             String customerName = customerNameText.getText();
             int addressId;
@@ -127,7 +138,7 @@ public class AddCustomerController implements Initializable {
             System.out.println("error: " + e);
 
         }
-
+        }
     }
 
     public Integer getCityIdByCity(String cityName) throws SQLException {
@@ -143,12 +154,163 @@ public class AddCustomerController implements Initializable {
         return null;
     }
 
+    public boolean validateCustomerName() {
+        String customer = customerNameText.getText();
+        if (customer.equals(null) || customer.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un nombre de cliente");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un nombre de cliente");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a Customer Name");
+                alert.showAndWait();
+                System.out.println("Please enter a Customer Name");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateAddress() {
+        String address = addressText.getText();
+        if (address.equals(null) || address.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor, introduzca una dirección en la línea de dirección 1");
+                alert.showAndWait();
+                System.out.println("Por favor, introduzca una dirección en la línea de dirección 1");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter an Address in Address Line 1");
+                alert.showAndWait();
+                System.out.println("Please enter an Address in Address Line 1");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateCity() {
+        String city = citySelection.getValue().toString();
+
+        if (city.equals(null) || city.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor seleccione una ciudad");
+                alert.showAndWait();
+                System.out.println("Por favor seleccione una ciudad");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please Select a City");
+                alert.showAndWait();
+                System.out.println("Please Select a City");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateZipCode() {
+        String ZipCode = zipCodeText.getText();
+        String regex = "^[0-9]{5}(?:-[0-9]{4})?$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ZipCode);
+
+        if (matcher.matches()) {
+            return true;
+
+        } else {
+            // some other code
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un código postal válido");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un código postal válido");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a valid zip code");
+                alert.showAndWait();
+                System.out.println("Please enter a valid zip code");
+            }
+            return false;
+        }
+    }
+
+    public boolean validatePhone() {
+        String ZipCode = phoneText.getText();
+        String regex = "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ZipCode);
+
+        if (matcher.matches()) {
+            return true;
+
+        } else {
+            // some other code
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un número de teléfono de 10 dígitos");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un número de teléfono de 10 dígitos");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a 10 digit Phone number");
+                alert.showAndWait();
+                System.out.println("Please enter a 10 digit Phone number");
+            }
+            return false;
+        }
+    }
+
+    public boolean validateAll() {
+
+        if (validateCustomerName()
+                && validateAddress()
+                && validateCity()
+                && validateZipCode()
+                && validatePhone()) {
+            return true;
+
+        } else {
+            // some other code
+            return false;
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         List<City> citylist = new ArrayList<>();
+        
+        
         try {
 
             citylist = DatabaseConnect.getAllCitys();
+            citySelection.setValue(citylist.get(0).getCity());
         } catch (SQLException ex) {
             System.out.println("error: " + ex);
         }
@@ -156,7 +318,7 @@ public class AddCustomerController implements Initializable {
         for (City var : citylist) {
             citySelection.getItems().addAll(var.getCity());
         }
-
+        
     }
 
 }
--- src/ViewsAndControllers/EditCustomerController.java
+++ src/ViewsAndControllers/EditCustomerController.java
@@ -15,13 +15,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
@@ -61,7 +65,7 @@ public class EditCustomerController implements Initializable {
 
     @FXML
     private Button cancelButton;
-    
+
     int cusId;
     int addrId;
 
@@ -72,14 +76,17 @@ public class EditCustomerController implements Initializable {
 
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
+    Locale mexicoLocale = new Locale("es", "MX");
+
+    //Changes default language english to testing language 
+    //Locale.setDefault(mexicoLocale);
+    Locale currentLocale = Locale.getDefault();
+
     /**
      * **********************************
      * Changing screens and scenes with buttons.
      * **********************************
      */
-    
-    
-    
     @FXML
     private void cancelButtonAction(ActionEvent event) throws IOException {
         stage = (Stage) cancelButton.getScene().getWindow();
@@ -89,48 +96,47 @@ public class EditCustomerController implements Initializable {
         stage.setScene(scene);
 
     }
-    
+
     @FXML
     private void saveButtonAction(ActionEvent event) throws IOException {
-try {
-            int userId = Login.getLoggedInUserId();
-            String customerName = customerNameText.getText();
-            int addressId=addrId;
-            String address = addressText.getText();
-            String address2 = address2Text.getText();
-            int active = 1;
-            int city;
+        if (validateAll()) {
+            try {
+                int userId = Login.getLoggedInUserId();
+                String customerName = customerNameText.getText();
+                int addressId = addrId;
+                String address = addressText.getText();
+                String address2 = address2Text.getText();
+                int active = 1;
+                int city;
 
-            city = getCityIdByCity(citySelection.getValue().toString());
-            String zipCode = zipCodeText.getText();
-            String phone = phoneText.getText();
+                city = getCityIdByCity(citySelection.getValue().toString());
+                String zipCode = zipCodeText.getText();
+                String phone = phoneText.getText();
 
-            String lastUpdate = timestamp.toString();
-            String lastUpdateby = Login.getLoggedInUser().getUserName();
+                String lastUpdate = timestamp.toString();
+                String lastUpdateby = Login.getLoggedInUser().getUserName();
 
-            
+                DatabaseConnect.editCustomer(cusId, customerName);
+                DatabaseConnect.editAddress(addressId, address, address2, city,
+                        zipCode, phone, lastUpdate, lastUpdateby);
 
-            DatabaseConnect.editCustomer(cusId, customerName);
-            DatabaseConnect.editAddress(addressId, address, address2, city, 
-                    zipCode, phone, lastUpdate, lastUpdateby);
+                // Save the appointment 
+                // Close the window 
+                stage = (Stage) saveButton.getScene().getWindow();
+                stage.hide();
+                CustomerSceenController.getActiveCustomerScreen().reinitializer();
 
-            // Save the appointment 
-            // Close the window 
-            stage = (Stage) saveButton.getScene().getWindow();
-            stage.hide();
-            CustomerSceenController.getActiveCustomerScreen().reinitializer();
+            } catch (SQLException e) {
+                // possibly show a popup with a try again or cancel option
 
-        } catch (SQLException e) {
-            // possibly show a popup with a try again or cancel option
+                System.out.println("error: " + e);
 
-            System.out.println("error: " + e);
-
+            }
         }
-
     }
 
     public void transferData(
-            int customerId,int addressId,
+            int customerId, int addressId,
             String customerName, String address, String address2,
             String citySelectionText, String zipCode, String phone) {
 
@@ -140,13 +146,12 @@ try {
         zipCodeText.setText(String.valueOf(zipCode));
         phoneText.setText(String.valueOf(phone));
         citySelection.setValue(String.valueOf(citySelectionText));
-        
-        cusId=customerId;
-        addrId=addressId;
+
+        cusId = customerId;
+        addrId = addressId;
     }
-    
-    
-        public Integer getCityIdByCity(String cityName) throws SQLException {
+
+    public Integer getCityIdByCity(String cityName) throws SQLException {
 
         List<City> citylist2 = new ArrayList<>();
         citylist2 = DatabaseConnect.getAllCitys();
@@ -159,14 +164,160 @@ try {
         return null;
     }
 
-        
-        
-        
+    public boolean validateCustomerName() {
+        String customer = customerNameText.getText();
+        if (customer.equals(null) || customer.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un nombre de cliente");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un nombre de cliente");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a Customer Name");
+                alert.showAndWait();
+                System.out.println("Please enter a Customer Name");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateAddress() {
+        String address = addressText.getText();
+        if (address.equals(null) || address.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor, introduzca una dirección en la línea de dirección 1");
+                alert.showAndWait();
+                System.out.println("Por favor, introduzca una dirección en la línea de dirección 1");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter an Address in Address Line 1");
+                alert.showAndWait();
+                System.out.println("Please enter an Address in Address Line 1");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateCity() {
+        String city = citySelection.getValue().toString();
+
+        if (city.equals(null) || city.isEmpty()) {
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor seleccione una ciudad");
+                alert.showAndWait();
+                System.out.println("Por favor seleccione una ciudad");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please Select a City");
+                alert.showAndWait();
+                System.out.println("Please Select a City");
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateZipCode() {
+        String ZipCode = zipCodeText.getText();
+        String regex = "^[0-9]{5}(?:-[0-9]{4})?$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ZipCode);
+
+        if (matcher.matches()) {
+            return true;
+
+        } else {
+            // some other code
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un código postal válido");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un código postal válido");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a valid zip code");
+                alert.showAndWait();
+                System.out.println("Please enter a valid zip code");
+            }
+            return false;
+        }
+    }
+
+    public boolean validatePhone() {
+        String ZipCode = phoneText.getText();
+        String regex = "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ZipCode);
+
+        if (matcher.matches()) {
+            return true;
+
+        } else {
+            // some other code
+            if (currentLocale == mexicoLocale) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Por favor ingrese un número de teléfono de 10 dígitos");
+                alert.showAndWait();
+                System.out.println("Por favor ingrese un número de teléfono de 10 dígitos");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Error");
+                alert.setContentText("Please enter a 10 digit Phone number");
+                alert.showAndWait();
+                System.out.println("Please enter a 10 digit Phone number");
+            }
+            return false;
+        }
+    }
+
+    public boolean validateAll() {
+
+        if (validateCustomerName()
+                && validateAddress()
+                && validateCity()
+                && validateZipCode()
+                && validatePhone()) {
+            return true;
+
+        } else {
+            // some other code
+            return false;
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+
         List<City> citylist = new ArrayList<>();
+
         try {
 
             citylist = DatabaseConnect.getAllCitys();
@@ -177,7 +328,7 @@ try {
         for (City var : citylist) {
             citySelection.getItems().addAll(var.getCity());
         }
-        
+
     }
 
 }
